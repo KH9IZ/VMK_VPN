8319e7c9eabca504da3a53606213fac3f7da09bf
Gennady <shutkov6914@gmail.com>
2022-06-14 12:00:42 +0300

    added comments and rewrote handler

--- main.py
+++ main.py
@@ -1,48 +1,68 @@
-import telebot
+"""
+VPN Bot main function, that declares it's workflow, supported commands and replies
+"""
+
 import os
+import telebot
 
 from wg import *
 
-conf = 'config'
 
 try:
     token = os.environ['vpn_bot_token']
-except:
-    exit()
+except Exception as exc:
+    print("Couldn't find VPN BOT token in environment variables. Please, set it!")
+    raise ModuleNotFoundError from exc
 
 bot = telebot.TeleBot(token)
 
 
-def gen_markup():
+def gen_markup(keys, row_width):
+    """
+    Create inline keyboard of given shape with buttons specified like callback:name in dict
+    """
+
     markup = telebot.types.InlineKeyboardMarkup()
-    markup.row_width = 1
-    markup.add(telebot.types.InlineKeyboardButton(
-        "Get your config!", callback_data=conf))
+    markup.row_width = row_width
+    for conf in keys:
+        markup.add(telebot.types.InlineKeyboardButton(
+            keys[conf], callback_data=conf))
     return markup
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = gen_markup()
+    """
+    Handler for /start command
+    """
+
+    markup = gen_markup({"config":  "Get your config!"}, 1)
     bot.send_message(chat_id=message.chat.id,
                      text="Welcome to the CMC MSU bot for fast and secure VPN connection!",
                      reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.callback_query_handler(func=lambda call: call.data == "config")
 def callback_query(call):
-    if call.data == conf:
-        doc = get_peer_config(call.from_user.id)
+    """
+    Callback handler to send user his config or to tell him that he doesn't have one
+    """
 
-        if doc:
-            bot.answer_callback_query(call.id, "Your config is ready!")
-            bot.send_document(chat_id=call.message.chat.id, document=doc)
-        else:
-            bot.answer_callback_query(
-                call.id, "No suitable config found. Sorry!")
+    doc = get_peer_config(call.from_user.id)
+
+    if doc:
+        bot.answer_callback_query(call.id, "Your config is ready!")
+        bot.send_document(chat_id=call.message.chat.id, document=doc)
+    else:
+        bot.answer_callback_query(
+            call.id, "No suitable config found. Sorry!")
 
 
 def main():
+    """
+    Start bot
+    """
+
     bot.infinity_polling()
 
 
