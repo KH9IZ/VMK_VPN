f56a6c80da526016953ef251989b40be8c752b0f
Gennady <shutkov6914@gmail.com>
2022-06-14 15:57:58 +0300

    new config generator

--- wg.py
+++ wg.py
@@ -1,22 +1,6 @@
-from telegram import *
-from telegram.ext import *
-from requests import *
-
-
-conf = "config"
-
-
-def getPeerConfig(update: Update, context: CallbackContext):
-    print(update._effective_user.id)
-    print(update.callback_query.data)
-    if conf in update.callback_query.data:
-        conf_name = str(update._effective_user.id)+".conf"
-        f = open(conf_name, 'wb')
-        f.write(b"Good job!")
-        f.close()
-
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Your config is ready!")
-        context.bot.send_document(chat_id=update.effective_chat.id,
-                                  document=open(conf_name, 'rb'), 
-                                  filename=conf_name)
+def get_peer_config(user_id):
+    try:
+        f = open(str(user_id) + '.conf', 'rb')
+        return f
+    except:    
+        return None
