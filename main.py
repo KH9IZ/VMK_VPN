45afa9d4e82331c68d3ec165c30ad652fdf5781f
Alex Poklonsky <jispar@turbanov.ru>
2022-06-19 08:16:50 +0300

    Refactor language settings.

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 """VPN Bot main function, that declares it's workflow, supported commands and replies."""
 
 import os
-import gettext
 import logging
+import typing
 import telebot
 import flag  # pip install emoji-country-flag
 
+from telebot.formatting import mbold
+from telebot.types import Message, CallbackQuery
 from wg import get_peer_config
 from models import QuestionAnswer, User
+from i18n_base_midddleware import I18N
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger('MainScript')
-from models import QuestionAnswer
 
 
-translations = {lang: gettext.translation("messages", "trans", fallback=True, languages=[lang])
-                for lang in ('ru', 'en')}
+class I18NMiddleware(I18N):
+    """Dynamic i18n."""
 
-def get_message_in_lang(text: str, lang:str = 'ru') -> str:
-    """Translate message with language."""
-    return translations[lang].gettext(text)
+    def process_update_types(self) -> list:
+        """List of update types which you want to be processed."""
+        return ['message', 'callback_query']
 
-_ = get_message_in_lang
+    def get_user_language(self, obj: typing.Union[Message, CallbackQuery]):
+        """Language will be used in 'pre_process' method of parent class."""
+        user_id = obj.from_user.id
+        lang = "ru" if (user := User.get_or_none(User.id == user_id)) is None else user.lang
+        return lang
+
+i18n = I18NMiddleware(translations_path='trans', domain_name='messages')
+_ = i18n.gettext
 
 
 try:
@@ -33,7 +42,8 @@ except Exception as exc:
     print(_("Couldn't find VPN BOT token in environment variables. Please, set it!"))
     raise ModuleNotFoundError from exc
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(token, use_class_middlewares=True)
+
 
 def gen_markup(keys: dict, row_width: int):
     """Create inline keyboard of given shape with buttons specified like callback:name in dict."""
@@ -48,115 +58,25 @@ def gen_markup(keys: dict, row_width: int):
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     """Menu for /start command."""
-    lang = "ru" if (user := User.get_or_none(User.id == message.chat.id)) is None else user.lang
-    markup = gen_markup({"config":  _("Get your config!", lang),
-                         "faq": _("FAQ", lang),
-                         "settings": _("Settings", lang)}, 1)
+    markup = gen_markup({"config":  _("Get your config!"),
+                         "faq": _("FAQ"),
+                         "settings": _("Settings")}, 1)
     bot.send_message(chat_id=message.chat.id,
                      text=_(
-                         "Welcome to the CMC MSU bot for fast and secure VPN connection!", lang),
+                         "Welcome to the CMC MSU bot for fast and secure VPN connection!"),
                      reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "config")
 def config_query(call):
     """Send user his config or tell him that he doesn't have one."""
-    # pylint: disable = unspecified-encoding
-    lang = ("ru" if (user := User.get_or_none(User.id == call.message.chat.id)) is None
-            else user.lang)
     if (doc := get_peer_config(call.from_user.id)):
-        bot.answer_callback_query(call.id, _("Your config is ready!", lang))
+        bot.answer_callback_query(call.id, _("Your config is ready!"))
         with open(doc, 'r') as config_file:
             bot.send_document(chat_id=call.message.chat.id, document=config_file)
     else:
         bot.answer_callback_query(
-            call.id, _("No suitable config found. Sorry!", lang))
-
-
-@bot.callback_query_handler(func=lambda call: call.data == 'faq')
-def faq_menu_query(call):
-    """Handle FAQ menu."""
-    lang = ("ru" if (user := User.get_or_none(User.id == call.message.chat.id))
-            is None else user.lang)
-    config: dict = {}
-    for question in QuestionAnswer.select():
-        config["faq_question_" + str(question.id)] = question.question
-    config["back_to_main_menu"] = _(" « Back", lang)
-    bot.edit_message_text(_("Frequently asked questions", lang), call.message.chat.id,
-                              call.message.message_id, reply_markup=gen_markup(config, 1))
-
-
-@bot.callback_query_handler(func=lambda call: call.data == 'settings')
-def settings_menu_query(call):
-    """Handle settings menu."""
-    lang = ("ru" if (user := User.get_or_none(User.id == call.message.chat.id))
-            is None else user.lang)
-    bot.edit_message_text(_("Settings", lang), call.message.chat.id,
-                          call.message.message_id,
-                          reply_markup=gen_markup({"change_language": _("Select language", lang),
-                                                   "back_to_main_menu": _(" « Back", lang)}, 1))
-
-
-@bot.callback_query_handler(func=lambda call: call.data == 'change_language')
-def change_language_menu_query(call):
-    """Handle change language settings menu."""
-    lang = ("ru" if (user := User.get_or_none(User.id == call.message.chat.id))
-            is None else user.lang)
-    config: dict = {}
-    for lang_name, flag_name in {"ru": "ru", "en": "gb"}.items():
-        config["change_lang_to_" + lang_name] = f"{flag.flag(flag_name)} {lang_name}"
-    config["settings"] = _(" « Back", lang)
-    bot.edit_message_text(_("Select your language:", lang), call.message.chat.id,
-                              call.message.message_id, reply_markup=gen_markup(config, 1))
-
-
-@bot.callback_query_handler(func=lambda call: call.data.startswith("change_lang_to_"))
-def change_user_language(call):
-    """Change user language."""
-    old_lang: str = None
-    new_lang: str = call.data.removeprefix("change_lang_to_")
-    if (user := User.get_or_none(User.id == call.message.chat.id)) is None:
-        old_lang = 'ru'  # Default one.
-        username = str(call.from_user.first_name + ' ' + call.from_user.last_name)
-        logger.info("Insert in user table new record with username: %s", username)
-        User.create(id=int(call.message.chat.id), username=username,
-                    lang=new_lang)
-    elif user.lang != new_lang:
-        old_lang = user.lang
-        user.lang = new_lang
-        logger.info("User table updated: %d", user.save())
-    bot.answer_callback_query(call.id, _("Language was changed to ", new_lang) + new_lang)
-    # Cant just call it: Telegram raise exception when you try to change text to the same one.
-    if old_lang is not None and old_lang != new_lang:
-        change_language_menu_query(call)
-
-
-@bot.callback_query_handler(func=lambda call: call.data.startswith("faq_question_"))
-def faq_question_query(call):
-    """Handle FAQ question button."""
-    lang = ("ru" if (user := User.get_or_none(User.id == call.message.chat.id))
-            is None else user.lang)
-    question_id = int(call.data.removeprefix("faq_question_"))
-    query = QuestionAnswer.get_by_id(question_id)
-    message_text = f"**{query.question}**\n\n{query.answer}"
-    bot.answer_callback_query(call.id, _("See your answer:", lang))
-    bot.edit_message_text(message_text, call.message.chat.id,
-                          call.message.message_id,
-                          reply_markup=gen_markup({"faq": _(" « Back", lang)}, 1),
-                          parse_mode="MARKDOWN")
-
-
-@bot.callback_query_handler(func=lambda call: call.data == "back_to_main_menu")
-def back_to_main_menu_query(call):
-    """Handle back to main menu button."""
-    lang = ("ru" if (user := User.get_or_none(User.id == call.message.chat.id)) is None else
-            user.lang)
-    markup = gen_markup({"config":  _("Get your config!", lang),
-                         "faq": _("FAQ", lang),
-                         "settings": _("Settings", lang)}, 1)
-    bot.edit_message_text(_("Welcome to the CMC MSU bot for fast and secure VPN connection!", lang),
-                          call.message.chat.id,
-                          call.message.message_id, reply_markup=markup)
+            call.id, _("No suitable config found. Sorry!"))
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'faq')
@@ -170,12 +90,57 @@ def faq_menu_query(call):
                               call.message.message_id, reply_markup=gen_markup(config, 1))
 
 
+@bot.callback_query_handler(func=lambda call: call.data == 'settings')
+def settings_menu_query(call):
+    """Handle settings menu."""
+    bot.edit_message_text(_("Settings"), call.message.chat.id,
+                          call.message.message_id,
+                          reply_markup=gen_markup({"change_language": _("Select language"),
+                                                   "back_to_main_menu": _(" « Back")}, 1))
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'change_language')
+def change_language_menu_query(call):
+    """Handle change language settings menu."""
+    if hasattr(call, 'new_lang'):
+        # You can manually set language by setting the attribute.
+        i18n.switch(call.new_lang)
+    config: dict = {}
+    for lang_name, flag_name in {"ru": "ru", "en": "gb"}.items():
+        config["change_lang_to_" + lang_name] = f"{flag.flag(flag_name)} {lang_name}"
+    config["settings"] = _(" « Back")
+    bot.edit_message_text(_("Select your language:"), call.message.chat.id,
+                              call.message.message_id, reply_markup=gen_markup(config, 1))
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("change_lang_to_"))
+def change_user_language(call):
+    """Change user language."""
+    new_lang: str = call.data.removeprefix("change_lang_to_")
+    if (user := User.get_or_none(User.id == call.message.chat.id)) is None:
+        old_lang = 'ru'  # Default one.
+        username = str(call.from_user.first_name + ' ' + call.from_user.last_name)
+        logger.info("Insert in user table new record with username: %s", username)
+        User.create(id=int(call.message.chat.id), username=username,
+                    lang=new_lang)
+    else:
+        old_lang = user.lang
+        if user.lang != new_lang:
+            user.lang = new_lang
+            logger.info("User table updated: %d", user.save())
+    bot.answer_callback_query(call.id, _("Language was changed to ", lang=new_lang) + new_lang)
+    # Cant just call it: Telegram raise exception when you try to change text to the same one.
+    if old_lang != new_lang:
+        setattr(call, 'new_lang', new_lang)
+        change_language_menu_query(call)
+
+
 @bot.callback_query_handler(func=lambda call: call.data.startswith("faq_question_"))
 def faq_question_query(call):
     """Handle FAQ question button."""
     question_id = int(call.data.removeprefix("faq_question_"))
     query = QuestionAnswer.get_by_id(question_id)
-    message_text = f"**{query.question}**\n\n{query.answer}"
+    message_text = f"{mbold(query.question)}\n\n{query.answer}"
     bot.answer_callback_query(call.id, _("See your answer:"))
     bot.edit_message_text(message_text, call.message.chat.id,
                           call.message.message_id,
@@ -187,7 +152,8 @@ def faq_question_query(call):
 def back_to_main_menu_query(call):
     """Handle back to main menu button."""
     markup = gen_markup({"config":  _("Get your config!"),
-                         "faq": _("FAQ")}, 1)
+                         "faq": _("FAQ"),
+                         "settings": _("Settings")}, 1)
     bot.edit_message_text(_("Welcome to the CMC MSU bot for fast and secure VPN connection!"),
                           call.message.chat.id,
                           call.message.message_id, reply_markup=markup)
@@ -195,6 +161,7 @@ def back_to_main_menu_query(call):
 
 def main():
     """Start bot."""
+    bot.setup_middleware(i18n)
     bot.infinity_polling()
 
 
